b5db954f8e5b3913082c44a9e4870bbf712fe826
Josh <chang101@aol.com>
2016-04-22 01:30:55 -0400

    Knight moves set *Still need to see if other teams piece is there, if so capture it*

--- src/pieces.rkt
+++ src/pieces.rkt
@@ -229,7 +229,63 @@
   (define (draw) 'T)
   
   (define (get-valid-moves board)
-    '())
+     ; moves default to null....might not need
+    (define moves '())
+
+    ; capture refs to X/Y coords....also might not need
+    (let ((x (call (call base 'get-tile) 'get-x))
+          (y (call (call base 'get-tile) 'get-y)))
+
+      ; begin accumulating moves
+      (begin
+
+               ; move down 3 left 1
+        (if (and (not (null? (call board 'tile-at (- x 1) (+ y 3))))
+                 (call (call board 'tile-at (- x 1) (+ y 3)) 'is-empty))
+            (set! moves (cons (call board 'tile-at (- x 1) (+ y 3)) moves))
+            void)
+
+               ; move down 3 right 1
+        (if (and (not (null? (call board 'tile-at (+ x 1) (+ y 3))))
+                 (call (call board 'tile-at (+ x 1) (+ y 3)) 'is-empty))
+            (set! moves (cons (call board 'tile-at (+ x 1) (+ y 3)) moves))
+            void)
+
+               ; move up 3 left 1
+        (if (and (not (null? (call board 'tile-at (- x 1) (- y 3))))
+                 (call (call board 'tile-at (- x 1) (- y 3)) 'is-empty))
+            (set! moves (cons (call board 'tile-at (- x 1) (- y 3)) moves))
+            void)
+        
+               ; move up 3 right 1
+        (if (and (not (null? (call board 'tile-at (+ x 1) (- y 3))))
+                 (call (call board 'tile-at (+ x 1) (- y 3)) 'is-empty))
+            (set! moves (cons (call board 'tile-at (+ x 1) (- y 3)) moves))
+            void)
+
+              ; move left 3 up 1
+        (if (and (not (null? (call board 'tile-at (- x 3) (- y 1))))
+                 (call (call board 'tile-at (- x 3) (- y 1)) 'is-empty))
+            (set! moves (cons (call board 'tile-at (- x 3) (- y 1)) moves))
+            void)
+
+              ; move left 3 down 1
+        (if (and (not (null? (call board 'tile-at (- x 3) (+ y 1))))
+                 (call (call board 'tile-at (- x 3) (+ y 1)) 'is-empty))
+            (set! moves (cons (call board 'tile-at (- x 3) (+ y 1)) moves))
+            void)
+
+               ; move right 3 up 1
+        (if (and (not (null? (call board 'tile-at (+ x 3) (- y 1))))
+                 (call (call board 'tile-at (+ x 3) (- y 1)) 'is-empty))
+            (set! moves (cons (call board 'tile-at (+ x 3) (- y 1)) moves))
+            void)
+
+               ; move right 3 down 1
+        (if (and (not (null? (call board 'tile-at (+ x 3) (+ y 1))))
+                 (call (call board 'tile-at (+ x 3) (+ y 1)) 'is-empty))
+            (set! moves (cons (call board 'tile-at (+ x 3) (+ y 1)) moves))
+            void))))
 
   (define (get-sprite)
     (if (eq? (call base 'get-team) white-team)
