82a577c0de7afb3dfcc801cb58075363b801aed1
Conor Finegan <conor_finegan@student.uml.edu>
2016-04-23 15:06:50 -0400

    new file for program enty point

---
+++ src/chess-main.rkt
@@ -0,0 +1,13 @@
+#lang racket
+
+;; chess-main.rkt
+;; Created on 4/23/16 at 3:05 PM
+;; Author: Conor Finegan
+;; Contact: conor_finegan@student.uml.edu
+;;
+;; Purpose: file containing entry point for the program
+
+(provide main)
+
+(define (main)
+  "Hello World!")
