e3ceee2fae3d71aeaf74474404f7e6f68c86df39
Conor Finegan <conor_finegan@student.uml.edu>
2016-04-26 18:15:22 -0400

    refactored for better separation of responsibilities

--- src/chess-main.rkt
+++ src/chess-main.rkt
@@ -9,12 +9,13 @@
 
 (provide main)
 
-(require "window.rkt")
-(require "teams.rkt")
-(require "board.rkt")
 (require "call.rkt")
-(require "move-result.rkt")
+(require "board.rkt")
 (require "pieces.rkt")
+(require "teams.rkt")
+(require "window.rkt")
+(require "board-canvas.rkt")
+(require "move-result.rkt")
 
 (define (main)
   (start-game))
@@ -23,15 +24,15 @@
 (define (start-game)
   (let ((board (make-board))
         (selected-tile '())
-        (player-turn white-team)
-        (print (λ (string-msg) (void))))
-
+        (moused-over-tile '())
+        (player-turn white-team))
+    
     ; swap to the next player's turn
     (define (swap-turns)
       (if (eq? player-turn white-team)
           (set! player-turn black-team)
           (set! player-turn white-team)))
-
+    
     ; when given a tile, attempt a selection if a piece
     ; has not already been selected. otherwise, attempt
     ; a move.
@@ -39,7 +40,7 @@
       (if (null? selected-tile)
           (select-tile tile)
           (attempt-move tile)))
-
+    
     ; attempts to select a tile, checks to make sure that the
     ; tile is a valid selection for the current player
     (define (select-tile tile)
@@ -52,7 +53,7 @@
             ; else select the tile
             (begin (set! selected-tile tile)
                    (print "Selected tile")))))
-
+    
     ; procedure for attempting to move a piece from selected-tile
     ; to dest-tile
     (define (attempt-move dest-tile)
@@ -62,7 +63,7 @@
           (set! selected-tile '())
           (process-captured-piece (move-result-piece result))
           (if (move-result-success? result) (swap-turns) void))))
-
+    
     ; for now only delcares a winner, doesn't do anyting else 
     (define (process-captured-piece piece)
       (if (null? piece)
@@ -70,7 +71,7 @@
           (if (eq? king% (call piece 'get-team))
               (declare-winner (call piece 'get-team))
               void)))
-
+    
     ; broken? won't print winner msg
     (define (declare-winner captured-team)
       (let ((winner-msg (string-append
@@ -78,20 +79,40 @@
                              "White player" "Black player")
                          " is the winner!")))
         (print winner-msg)))
-
-    ; callback for mouse clicks into the window board
-    ; x/y params are in terms of tiles, not pixels
+    
+    ; procedure for printing a string to the window's output-msg
+    (define (print string-msg)
+      (send window set-message string-msg))
+    
+    ; callback for mouse click
     (define (on-mouse-click tile-x tile-y)
       (let ((tile (call board 'tile-at tile-x tile-y)))
         (click-tile tile)))
-          
 
+    ; callback for mouse over
     (define (on-mouse-over tile-x tile-y)
-      void)
-
-    ; create display window and override print procedure to hook into
-    ; window's output message
-    (set! print (create-window board on-mouse-click on-mouse-over))))
+      (let ((tile (call board 'tile-at tile-x tile-y)))
+        (if (not (eq? tile moused-over-tile))
+            (begin (send board-canvas refresh-now)
+                   (send board-canvas highlight-tile tile-x tile-y "brown")
+                   (set! moused-over-tile tile))
+            void)))
+    
+    ; callback for canvas paint
+    (define (on-canvas-paint canvas dc)
+      (map
+       (λ (tile) (send canvas draw-tile tile (eq? tile selected-tile)))
+       (call board 'get-all-tiles)))
+    
+    ; create window and canvas resources, then present
+    ; window to user
+    (begin
+      (define window (make-window))
+      (define board-canvas (make-board-canvas
+                            window
+                            on-mouse-click
+                            on-mouse-over
+                            on-canvas-paint))
+      (send window show #t))))
 
 (main)
-
--- src/window.rkt
+++ src/window.rkt
@@ -10,103 +10,40 @@
 (require racket/gui)
 (require racket/draw)
 
-(require "board.rkt")
 (require "call.rkt")
-(require "move-result.rkt")
+(require "board-canvas.rkt")
 
-(provide create-window)
+(provide make-window)
 
-(define (create-window
-         board 
-         mouse-click-proc ; needs to take 2 params (tile-x, tile-y)
-         mouse-over-proc  ; needs to take 2 params (tile-x, tile-y)
-         ; paint-proc ; λ (canvas dc)
-         )
+; closure that captures a chess-frame% construction
+; and returns it to the caller
+(define (make-window)
 
-  ; constant for tile dimension (75x75)
-  (define tile-bmp-size 75)
-  
-  ; top-level window
+  (define chess-frame%
+    (class frame%
+
+      (define/public (set-message string-msg)
+        (send output-msg set-label string-msg))
+
+      ; end of chess-frame% definition
+      (super-new)))
+
+  ; instance of top-level window
   (define frame
-    (new frame%
+    (new chess-frame%
          (label "ChessNuts")
          (width 600)
          (height 600)
          (style (list 'no-resize-border))))
-  
-  ; panel for user controls and output text
-  (define output-panel
-    (new horizontal-panel%
-         (parent frame)))
-  
-  ; string representing last message sent to user
+
+  ; string representing last message sent to the user
   (define output-msg
+    
     (new message%
-         (parent output-panel)
-         (label "No events so far...")))
-  
-  ; board-canvas is the canvas for the board display
-  (define board-canvas%
-    (class canvas%
-      (inherit refresh-now)
+         (parent frame)
+         (label "No events so far...")
+         (min-width 300)
+         (font (make-object font% 15 'default))))
 
-      ; mouse event callback
-      (define/override (on-event event)
-        (let ((mouse-x (send event get-x))
-              (mouse-y (send event get-y)))
-          (if (send event button-down?)
-              ; if event was button down, call into mouse-click-proc
-              (let ((tile-x (floor (/ mouse-x tile-bmp-size)))
-                    (tile-y (floor (/ mouse-y tile-bmp-size))))
-                (begin (mouse-click-proc tile-x tile-y) (refresh-now)))
-              ; else do nothing
-              void)))
-              
-      (super-new)))
-  
-  ; procedure for drawing the board
-  (define (draw-board dc board)
-    (let ((white-tile-bmp (read-bitmap "../Images/White_Tile.png"))
-          (black-tile-bmp (read-bitmap "../Images/Black_Tile.png")))
-      (map
-       (λ (tile)
-         (let ((x (call tile 'get-x))
-               (y (call tile 'get-y)))
-           (begin
-             ; draw tile
-             (send dc draw-bitmap
-                   (if (even? (+ x y)) white-tile-bmp black-tile-bmp)
-                   (* x tile-bmp-size)
-                   (* y tile-bmp-size))
-             ; draw piece
-             (if (not (call tile 'is-empty))
-                 (draw-piece (call tile 'get-piece) dc x y)
-                 void))))
-
-       (call board 'get-all-tiles))))
-  
-  ; method to draw an individual piece
-  (define (draw-piece piece dc x y)
-    (send dc draw-bitmap
-          (call piece 'get-sprite)
-          (+ (* tile-bmp-size x) 0)
-          (+ (* tile-bmp-size y) 0)))
-
-  ; creates the board-canvas inside frame
-  (new board-canvas%
-       (parent frame)
-       (min-width 600)
-       (min-height 600)
-       
-       ; callback for drawing board
-       (paint-callback
-        (λ (canvas dc)
-          (draw-board dc board))))
-
-  ; show frame
-  (send frame show #t)
-
-
-  ; return simple object for modifying output-msg
-  (λ (string-msg)
-    (send output-msg set-label string-msg)))
+  ; return frame to user
+  frame)
