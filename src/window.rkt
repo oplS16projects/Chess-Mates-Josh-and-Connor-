80fa3366d04c53d152b2aee21db9f4c219650cf6
unknown <Conor_Finegan@OS308-04.fs.uml.edu>
2016-04-29 11:23:59 -0400

    added castling button and public method to enable or disable it

--- src/window.rkt
+++ src/window.rkt
@@ -18,18 +18,24 @@
 ; closure that captures a chess-frame% construction
 ; and returns it to the caller
 (define (make-window
-         reset-proc ; λ -> (button event)
+         reset-proc  ; λ -> (button event)
+         castle-proc ; λ -> (button event)
          )
 
   (define chess-frame%
     (class frame%
 
+      ; public method to set output message
       (define/public (set-message string-msg)
         (send output-msg set-label string-msg))
+      
+      ; public method to set availability of castling button
+      (define/public (set-castle-button on?)
+        (send castle-button enable on?))
 
       ; end of chess-frame% definition
       (super-new)))
-
+  
   ; instance of top-level window
   (define frame
     (new chess-frame%
@@ -37,20 +43,28 @@
          (width 600)
          (height 600)
          (style (list 'no-resize-border))))
-
+  
   ; horizontal panel to hold output-msg and reset button
   (define panel
     (new horizontal-panel%
          (parent frame)))
-
+  
   ; string representing last message sent to the user
   (define output-msg
     (new message%
          (parent panel)
          (label "Welcome to ChessNuts")
-         (min-width 525)
+         (min-width 455)
          (font (make-object font% 15 'default))))
-
+  
+  ; button for castling
+  (define castle-button
+    (new button%
+         (parent panel)
+         (label "Castle")
+         (callback castle-proc)
+         (enabled #f)))
+  
   ; button for reseting the game
   (new button%
        (parent panel)
